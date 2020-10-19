b4b0841670bc2d26b16dca75a6f829bb09cdf9e3
R <R>
2020-10-19 15:56:35 -0400

    core-clean-up

--- model/ImageFile.py
+++ model/ImageFile.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
 from osgeo import gdal
@@ -22,18 +21,30 @@ class ImageFile(BaseFile):
         # Initialize the base class.
         super(ImageFile, self).__init__(pathToFile)
 
+        # ---
+        # Corrupt files tend to fail when they are opened in _getDataset(),
+        # so try it here and fail early.
+        # ---
         self._dataset = None
+
+        try:
+            self._dataset = gdal.Open(self._filePath, gdalconst.GA_ReadOnly)
+                                      
+            if not self._dataset:
+                
+                raise RuntimeError('GDAL returned an null data set ' + \
+                                   'when opening ' + \
+                                   self._filePath + \
+                                   '.')
+
+        except:
+            
+            raise RuntimeError('GDAL raised an exception when opening ' + \
+                               self._filePath + '.')
 
     # -------------------------------------------------------------------------
     # _getDataset
     # -------------------------------------------------------------------------
     def _getDataset(self):
-
-        if not self._dataset:
-
-            self._dataset = gdal.Open(self._filePath, gdalconst.GA_ReadOnly)
-
-            if not self._dataset:
-                raise RuntimeError('Unable to read ' + self._filePath + '.')
 
         return self._dataset
